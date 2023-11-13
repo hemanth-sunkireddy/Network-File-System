69924fe4f7fa6ade92020579959dc46fa11820e8
codepk37 <111884178+codepk37@users.noreply.github.com>
2023-11-13 15:49:33 +0530

    Update client.c

--- client/client.c
+++ client/client.c
@@ -1,12 +1,22 @@
 #include "../headers.h"
 
 
-char *ip = "127.0.0.1";
-int port = PORT;
+// char *ip;// = "127.0.0.1";
+int port=12345;// = PORT;
 char message[MAX_LENGTH];
 
+typedef struct fileNameAndOperation{
+    int operation_number; 
+    char name_of_file_or_folder[MAX_LENGTH];
+}fileNameAndOperation;
+
+typedef struct NS{
+    int PORT;
+    char IP[100];
+}NS_reply;
 
 void Info(fileNameAndOperation *file_or_folder_detail){
+
 
         // fileNameAndOperation file_or_folder_details;
 
@@ -38,10 +48,7 @@ void Info(fileNameAndOperation *file_or_folder_detail){
         
         // send(client_socket, &file_or_folder_details, MAX_LENGTH, 0);
 }
-typedef struct fileNameAndOperation{
-    int operation_number; 
-    char name_of_file_or_folder[MAX_LENGTH];
-}fileNameAndOperation;
+
 typedef struct acknowledgmentMessage{
     int operation_number;
     char status_message[MAX_LENGTH];
@@ -49,32 +56,30 @@ typedef struct acknowledgmentMessage{
     folderInformation folder_information;
 }acknowledgmentMessage;
 
-typedef struct NS{
-    int PORT;
-    int IP;
-}NS_reply;
+
 
 
 int main(){
-
-  
-  
+      
 
    while(1){
+    char ip[100] = "127.0.0.1";
+    port = PORT;
+
     int client_socket;
     struct sockaddr_in client_address;
     socklen_t client_address_size;
     char buffer[MAX_LENGTH];
 
           
-          // Creating socket for connecting to server.
+          // Creating socket for connecting to Naming server.
           client_socket = socket(AF_INET, SOCK_STREAM, 0);
           if (client_socket < 0){
             perror("Socket error");
             exit(1);
           }
           else{
-            printf("TCP CLIENT SOCKET CREATED.\n");
+            printf("TCP CLIENT SOCKET CREATED for NS.\n");
           }
 
           memset(&client_address, '\0', sizeof(client_address));
@@ -90,7 +95,7 @@ int main(){
           // Send the request to server
            fileNameAndOperation file_or_folder_details;
            Info(&file_or_folder_details);
-           send(client_socket, &file_or_folder_details, MAX_LENGTH, 0);
+           send(client_socket, &file_or_folder_details, sizeof(file_or_folder_details), 0);
 
         //   int op=send_client_request(client_socket);
         ///////
@@ -101,32 +106,82 @@ int main(){
           if(file_or_folder_details.operation_number==3 ||file_or_folder_details.operation_number==4 ||file_or_folder_details.operation_number==8 ||file_or_folder_details.operation_number==9){
              char message_status[MAX_LENGTH];
             recv(client_socket, message_status, MAX_LENGTH, 0);
+             
             printf("OPERATION STATUS:%s\n", message_status);
+            close(client_socket);
+            printf("Disconnected from the Naming Server.\n");
           }
           else{//1,2,5,6,7
              //
              NS_reply reply;
-             recv(client_socket, reply, sizeof(reply), 0);
+             recv(client_socket, &reply, sizeof(reply), 0);
               close(client_socket);
              printf("Disconnected from the Naming Server.\n");
              // Close the socket.
-
-
+            
              //create socket for Storage Server
+             strcpy(ip, reply.IP);
+            port = reply.PORT;
+
+            int client_socket;
+            struct sockaddr_in client_address;
+            socklen_t client_address_size;
+            //
+
+             // Creating socket for connecting to Naming server.
+            client_socket = socket(AF_INET, SOCK_STREAM, 0);
+            if (client_socket < 0){
+                perror("Socket error");
+                exit(1);
+            }
+            else{
+                printf("TCP CLIENT SOCKET CREATED for SS.\n");
+            }
+
+            memset(&client_address, '\0', sizeof(client_address));
+            client_address.sin_family = AF_INET;
+            client_address.sin_port = port;
+            client_address.sin_addr.s_addr = inet_addr(ip);
+
+            // Connect to the SS
+            connect(client_socket, (struct sockaddr*)&client_address, sizeof(client_address));
+            printf("Connected to the SS\n");
+
+
+             send(client_socket, &file_or_folder_details, sizeof(file_or_folder_details), 0);
+            printf("\nWait till the response from SS\n");
+
+            
+            acknowledgmentMessage ack_fromSS;
+            int flag=1;
+            while(1){
+             recv(client_socket,&ack_fromSS ,sizeof(ack_fromSS), 0);
+             //print 
+            //              char status_message[MAX_LENGTH];
+            // Packet file_or_folder_content[MAX_LENGTH];
+            // folderInformation folder_information;
+                 printf("%s",ack_fromSS.status_message);
+
+                 if(strcmp(ack_fromSS.status_message,"STOP")==0){
+                    flag=0;
+                 }
+
+                 //--** PRINT folder_information
+                 //--** PRINT  file_or_folder_content[MAX_LENGTH]
+            
+            
+            }
+
+            //STOP
+            close(client_socket);
+            printf("Disconnected from the Storage Server.\n");
+
+            
 
 
 
 
-
-
-
-          }
-
-        
-         
-
-          
-         
+          }        
  
 
           
@@ -137,3 +192,4 @@ int main(){
   return 0;
 
 }
+
