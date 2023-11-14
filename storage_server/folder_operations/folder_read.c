fbb1f0d328b1f83d515e523f2c329bcd87a706ff
Hemanth Sunkireddy <117459441+hemanth-sunkireddy@users.noreply.github.com>
2023-11-14 12:46:49 +0530

    Updated listing of folder in acknowledgment message

--- storage_server/folder_operations/folder_read.c
+++ storage_server/folder_operations/folder_read.c
@@ -16,15 +16,17 @@ acknowledgmentMessage listing_all_files_and_folders(char folder_name[MAX_LENGTH]
 
     
     printf("Files and folders in %s:\n", folder_name);
-
+    int total_count_of_files_or_folders = 0;
     while ((entry = readdir(dir)) != NULL) {
         if (entry->d_type == DT_DIR) {
             printf("Folder: %s\n", entry->d_name);
         } else {
             printf("File:%s\n", entry->d_name);
         }
+        strcpy( message_status.list_of_folder[total_count_of_files_or_folders].name_of_file_or_folder, entry->d_name);
+        total_count_of_files_or_folders++;
     }
-
+    message_status.list_of_folder.total_number_of_files_or_folders = total_count_of_files_or_folders;
     closedir(dir);
     }
 
