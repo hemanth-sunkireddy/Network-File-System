233d7a3e9feea6b5f0862b9e7c6e91e95586f24f
SreejaP11 <psreeja6688@gmail.com>
2023-11-17 23:40:43 +0530

    Updated nm_receivingRequest.c

--- naming_server/nm_receivingRequest.c
+++ naming_server/nm_receivingRequest.c
@@ -399,9 +399,9 @@ void ReadorWriteOperation(SS_Info ssx[MAX_STORAGE_SERVERS], acknowledgmentMessag
     strcpy(path,FilenameAndOperation.name_of_file_or_folder);
     printf("Path name: %s",path);
     
-    // for(int i=0;i<number_of_storage_servers;i++){
-    //     for(int j=0;j<ssx[i].num_of_files+ssx[i].num_of_folders;j++){
-    //         if((strncmp(path,ssx[i].paths_accessible[j].path,strlen(ssx[i].paths_accessible[j].path))==0 && path[strlen(ssx[i].paths_accessible[j].path)]=='\0') || (strncmp(path,ssx[i].paths_accessible[j].path,strlen(ssx[i].paths_accessible[j].path))==0 && path[strlen(ssx[i].paths_accessible[j].path)]=='/' && path[strlen(ssx[i].paths_accessible[j].path) + 1] != '\0')) {    
+    for(int i=0;i<number_of_storage_servers;i++){
+        for(int j=0;j<ssx[i].num_of_files+ssx[i].num_of_folders;j++){
+            if((strncmp(path,ssx[i].paths_accessible[j].path,strlen(ssx[i].paths_accessible[j].path))==0 && path[strlen(ssx[i].paths_accessible[j].path)]=='\0') || (strncmp(path,ssx[i].paths_accessible[j].path,strlen(ssx[i].paths_accessible[j].path))==0 && path[strlen(ssx[i].paths_accessible[j].path)]=='/' && path[strlen(ssx[i].paths_accessible[j].path) + 1] != '\0')) {    
                 Data_of_SS_SentToClient data_sent;
                 data_sent.SS_port=ssx[0].SS_port;
                 printf("ASSIGNED PORT SUCCESS.");
@@ -418,29 +418,29 @@ void ReadorWriteOperation(SS_Info ssx[MAX_STORAGE_SERVERS], acknowledgmentMessag
                 struct sockaddr_in client_address;
                 socklen_t client_address_size;
                 printf("HELLO");
-                //         if (client_socket < 0){
-                //             perror("Socket error");
-                //             exit(1);
-                //         }
-                //         else{
-                //             printf("TCP CLIENT SOCKET CREATED for NS.\n");
-                //         }
-                //         memset(&client_address, '\0', sizeof(client_address));
-                //         client_address.sin_family = AF_INET;
-                //         client_address.sin_port = port;
-                //         client_address.sin_addr.s_addr = inet_addr(ip);
-                //         //printf("%d %d %d %d\n",ssx[i].SS_port,ssx[i].storage_server_number,ssx[i].num_of_files,ssx[i].num_of_folders);
-                //         // Connect to the server
-                //         connect(client_socket, (struct sockaddr*)&client_address, sizeof(client_address));
-                //         printf("Connected to the client.\n\n");
-                        char message[1024];
-                        strcpy(message, "HELLO ECHO FROM NAMING SERVER.");
-                send(ss_socket,message,MAX_LENGTH,0);
-                
+                        if (client_socket < 0){
+                            perror("Socket error");
+                            exit(1);
+                        }
+                        else{
+                            printf("TCP CLIENT SOCKET CREATED for NS.\n");
+                        }
+                        memset(&client_address, '\0', sizeof(client_address));
+                        client_address.sin_family = AF_INET;
+                        client_address.sin_port = port;
+                        client_address.sin_addr.s_addr = inet_addr(ip);
+                        //printf("%d %d %d %d\n",ssx[i].SS_port,ssx[i].storage_server_number,ssx[i].num_of_files,ssx[i].num_of_folders);
+                        // Connect to the server
+                        connect(client_socket, (struct sockaddr*)&client_address, sizeof(client_address));
+                        printf("Connected to the client.\n\n");
+                //         char message[1024];
+                //         strcpy(message, "HELLO ECHO FROM NAMING SERVER.");
+                // send(ss_socket,message,MAX_LENGTH,0);
+                send(ss_socket,&data_sent,sizeof(Data_of_SS_SentToClient),0);
                 printf("Sent File or folder name and Operation number successfully\n");
-    //         }
-    //     }
-    // }
+            }
+        }
+    }
 }
 
 acknowledgmentMessage obtain_ss_info(SS_Info ssx[MAX_STORAGE_SERVERS],int ss_socket, acknowledgmentMessage message_status, fileNameAndOperation operation_and_fileorfolder_name){
